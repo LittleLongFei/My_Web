969a3ae59954d3c76ab0dc0d39e8c01ae894ac25
H. Zhang <115519022+LittleLongFei@users.noreply.github.com>
2023-12-03 21:24:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,11 @@ st.set_page_config(
 )
 
 
-st.sidebar.success("The copyright belongs to Shandong University")
-st.sidebar.info("Developer: H.Zhang")
-st.sidebar.error("Created Time: 2023-5-23")
+# 头像
+image = Image.open(r'./Resource/1.png')
+st.image(image)
+
+
 
 col1, col2, col3, col4 = st.columns([1,1,1,1.5])
 
@@ -27,6 +29,6 @@ with col3:
 col1, col2, col3, col4 = st.columns([1,1,1.1,1.5])
 
 with col3:
-    st.caption('Copyright © 2023, 山东大学 中国·济南')
+    st.caption('Copyright @ 2023, 山东大学 中国·济南')
 
 
