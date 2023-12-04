be9405cf849ac61e2870afe1f94c30d86d78a75b
H. Zhang <115519022+LittleLongFei@users.noreply.github.com>
2023-12-04 17:25:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 
 
-
-
-
 import streamlit as st
 
 from PIL import Image
@@ -17,22 +14,25 @@ st.set_page_config(
 
 
 # 头像
-col11, col12, col13, col14 = st.columns([1,1,1.5,1.5])
+col11, col12, col13, col14 = st.columns([1, 1, 1.5, 1.5])
 with col13:
     image = Image.open(r'./Resource/1.png')
     st.image(image, width=300)
 
 st.markdown("# Hui-Zhang")
-    
-st.markdown("Shandong University, Jinan, China. ")
+
+st.markdown("山东大学 控制科学与工程学院, 中国, 济南. ")
 st.markdown("E-mail: 202234949@mail.sdu.edu.cn, bigserendipty@gmail.com")
 
 st.success("Research Interest")
-st.markdown("Machine learning, Deep learning, Contrastive learning, Neural network architecture search, Reinforcement learning...")
-st.markdown("Convolutional neural network(CNN), Long short-term memory network(LSTM), Residual neural network(RNN), Broad learning system(BLS), Interval type-2 fuzzy logic system(IT2FLS), TSK fuzzy logic system, Deep fuzzy system(DFS), Transformer, Encoder-decoder architecture...")
-st.markdown("Adaptive optimization algorithm, Evolutionary algorithm, Gradient descent algorithm, Forward automatic differentiation technology, Backpropagation algorithm...")
-st.markdown("Long-term forecasting of new energy and load, Integrated energy system planning and design, Integrated energy system optimization control...")
-st.markdown("Software development, Hardware design, Algorithm design, Vue framework, SpringBoot framework, PyQt5 framework, Streamlit framework, JAX framework, PyTorch framework...")
+st.markdown("机器学习, 深度学习, 对比学习, 神经架构搜索, 强化学习, 迁移学习, 域适应, 随机特征, 科学机器学习...")
+st.markdown("卷积神经网络(CNN), 长短期记忆网络(LSTM), 残差神经网络(RNN), 宽度学习系统(BLS), 区间二型模糊逻辑系统(IT2FLS), TSK模糊逻辑系统, 深度模糊系统(DFS), Transformer, 物理信息神经网络...")
+st.markdown("自适应优化算法, 进化算法, 梯度下降算法, 前向自动微分, 反向传播算法...")
+st.markdown(
+    "新能源—负荷长序列预测, 综合能源系统规划设计, 综合能源系统优化控制, 机器学习回归任务, 机器学习分类任务, 机器学习聚类任务...")
+st.markdown(
+    "软件开发, 硬件设计, 算法实现, Vue框架, SpringBoot框架, PyQt5框架, Streamlit框架, JAX框架, PyTorch框架...")
+st.markdown("C语言, C++, Python, Java, Java Web...")
 st.success("Research Articles")
 
 st.markdown("[1] **H.Zhang**, W.Peng. Time Series Forecasting Based on Interval Type-2 Fuzzy Logic System with PSO, 2021 China Automation Congress (CAC), Beijing, China, 2021, pp. 3090-3097, doi: 10.1109/CAC53003.2021.9727414.")
@@ -46,21 +46,23 @@ st.success("Patent")
 st.markdown("[1] 孙波, **张辉**, 张立志, 一种集成结构—容量—运行的综合能源系统架构搜索方法, ***国家发明专利***")
 
 st.success("Standard")
-st.markdown("[1] 潘凤文, 孙波, 张立志, 张全军, 刘洋, **张辉**, 吴睿, 杨锋, 王迎波, 秦顺顺, 冯美丽, 含氢分布式综合能源系统运行优化指南, ***地方/行业标准***")
+st.markdown(
+    "[1] 潘凤文, 孙波, 张立志, 张全军, 刘洋, **张辉**, 吴睿, 杨锋, 王迎波, 秦顺顺, 冯美丽, 含氢分布式综合能源系统运行优化指南, ***地方/行业标准***")
 
 st.success("Participate In Projects")
-st.markdown("**国家重点研发计划项目**, 管道氢气在城镇供能领域关键技术研究与规模应用, **项目骨干**, 经费**5.15亿**元")
-st.markdown("**国家重点研发计划项目**, 氢能多场景深度融合及智慧管控技术研究与示范应用, **项目骨干**, 经费**2.05亿**元")
-st.markdown("**国家电网科技项目**, 多能互补综合能源系统协同优化控制研究, **项目骨干**, 经费**300万**元")
+st.markdown(
+    "[1] **国家重点研发计划项目**, 管道氢气在城镇供能领域关键技术研究与规模应用, **项目骨干**, 经费**5.15亿**元")
+st.markdown(
+    "[2] **国家重点研发计划项目**, 氢能多场景深度融合及智慧管控技术研究与示范应用, **项目骨干**, 经费**2.05亿**元")
+st.markdown("[3] **国家电网科技项目**, 多能互补综合能源系统协同优化控制研究, **项目骨干**, 经费**300万**元")
 
-col1, col2, col3, col4 = st.columns([1,1,1,1.5])
+col1, col2, col3, col4 = st.columns([1, 1, 1, 1.5])
 
 with col3:
     image = Image.open(r'./Resource/4.png')
     st.image(image)
 
-col1, col2, col3, col4 = st.columns([1,1,1.1,1.5])
+col1, col2, col3, col4 = st.columns([1, 1, 1.1, 1.5])
 
 with col3:
     st.caption('Copyright @ 2023, 山东大学 中国·济南')
-
